3d4f057ca0099bc1417191bca83488f8e4f6425a
Elijah Manor <elijah.manor@daveramsey.com>
2013-12-02 17:21:51 -0600

    Fully support IE8 by converting NodeList to an array and providing a search function

--- dist/echo.js
+++ dist/echo.js
@@ -21,15 +21,32 @@ window.Echo = (function (window, document, undefined) {
       var self = store[i];
       if (_inView(self)) {
         self.src = self.getAttribute('data-echo');
-        if ([].indexOf && [].slice.call(store).indexOf(self) !== -1) {
-          [].slice.call(store).splice(i, 1);
+        if (_hasItem(store, self)) {
+          store.splice(i, 1);
+          console.log('store.length: ' + store.length);
         }
       }
     }
   };
 
+  var _hasItem = function(items, item) {
+    var found = false;
+    for (var i = 0, len = items.length; i < len && !found; i++) {
+      if (items[i] === item) { found = true; }
+    }
+    return found;
+  };
+
+  var _toArray = function(nodes) {
+    var array = [];
+    for (var i = 0, len = nodes.length; i < len; i++) {
+      array.push(nodes[i]);
+    }
+    return array;
+  };
+
   var init = function () {
-    store = document.querySelectorAll('[data-echo]');
+    store = _toArray(document.querySelectorAll('[data-echo]'));
     _pollImages();
     window.onscroll = _pollImages;
   };
